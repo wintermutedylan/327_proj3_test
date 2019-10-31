f45917e8c63aea28eb4f87de8236aedb57542545
Dylan Wintermute <dylan.wintermute@cnu.edu>
2019-10-31 17:37:47 -0400

    score = 75 working on find tag and get data between tags

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -23,7 +23,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -I"/home/keith/git/327_proj3_stringparser" -I/327_proj3_fileIO -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++0x -I"/home/dylan/git/327_proj3_stringparser" -I"/home/dylan/git/327_proj3_fileIO" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
